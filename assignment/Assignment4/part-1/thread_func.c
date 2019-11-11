cd0db4c740f18a1ad12e18645f4fed1429b83413
Ritesh Kumar <ritesh99rakesh@gmail.com>
2019-11-11 18:55:20 +0530

    Part 1 working

--- assignment/Assignment4/part-1/thread_func.c
+++ assignment/Assignment4/part-1/thread_func.c
@@ -1,29 +1,33 @@
 #include<pthread.h>
 #include "common.h"
+#include<stdio.h>
 
 /*TODO:  
-     Insert approproate synchronization statements
-     to make it work with multiple threads
+	 Insert approproate synchronization statements
+	 to make it work with multiple threads
 */
 void *hashit(void *arg)
 /*Argument is the end pointer*/
 {
-   char *cptr;
-   unsigned long *chash;
-   char *endptr = (char *)arg;   // END pointer
+	char *cptr;
+	unsigned long *chash;
+  char *endptr = (char *)arg;   // END pointer
 
-   while(1){
-        if(dataptr >= endptr){
-              break;
-        }
-        cptr = dataptr;
-        dataptr += BLOCK_SIZE;
-        
-        chash = optr;
-        optr++;
-   
-        /*   Perform the real calculation. The following line should not be inside any locks*/
-        *chash = calculate_and_store_hash(cptr, endptr); 
+  while(1){
+	pthread_mutex_lock(&lock);
+  	if(dataptr >= endptr){
+  		pthread_mutex_unlock(&lock);
+  		break;
+  	}
+  	cptr = dataptr;
+  	dataptr += BLOCK_SIZE;
+
+  	chash = optr;
+  	optr++;
+  	pthread_mutex_unlock(&lock);
+
+		/*   Perform the real calculation. The following line should not be inside any locks*/
+  	*chash = calculate_and_store_hash(cptr, endptr); 
   }
   pthread_exit(NULL); 
 }
